3165147a5c60b6f92358ae6b51aae2dba0677756
Tobias Östh <99732913+TobbsProjects@users.noreply.github.com>
2022-10-16 21:58:44 +0200

    Project 3 - meme generatior
    Added Navbar/Header/Banner to site.

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Navbar.css'
+import Logo from './assets/trollface.png'
+
+export default function Navbar(){
+    return (
+        <nav className='nav--container'>
+            <div className='nav--logo'>
+                <img src={Logo} className='nav--image' alt='Logo' />
+                <h1 className='nav--logo--text'>Meme Generator</h1>
+            </div>
+            <h3 className='nav--project-name'>React Course - Project 3</h3>
+        </nav>
+    )
+}
